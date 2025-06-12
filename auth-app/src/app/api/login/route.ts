c016acbf63305b5357177b7a187f76f82b6bf8b9
Adam Giesey <adam.giesey@lcecorp.com>
2025-06-12 14:57:55 -0400

    auth app adds the cookie when hitting the login route

--- auth-app/src/app/api/login/route.ts
+++ auth-app/src/app/api/login/route.ts
@@ -1,5 +1,24 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  return NextResponse.json({ message: "hello, Adam. You are logged in" });
+export async function GET(request: Request) {
+  const expires = new Date(Date.now() + 60 * 60 * 1000);
+
+  const host = request.headers.get('host') || '';
+  
+  const domain = host.split('.').length > 2
+    ? '.' + host.split('.').slice(-2).join('.')
+    : '.' + host;
+
+  const response = NextResponse.json({ message: `login to ${domain} successful` });
+  
+  response.cookies.set('auth_token', 'authenticated', {
+    domain,
+    path: '/',
+    expires,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'lax'
+  });
+
+  return response;
 }
